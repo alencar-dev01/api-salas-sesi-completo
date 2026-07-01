a6714122817206cda8c6901a0a8aa1374c8f3ec1
Ricardo Antônio Santos Alencar <ricardo.alencar@aluno.senai.br>
2026-07-01 10:45:33 -0300

    fix: remove funcionalidade e modelos de chamados

--- models/index.js
+++ models/index.js
@@ -1,24 +1,7 @@
-// // models/index.js - Centraliza modelos e associações
-// const sequelize = require('../config/database');
-// const User = require('./User');
-// const Sala = require('./Sala');
-// const Reserva = require('./Reserva');
-
-// // Associações
-// User.hasMany(Reserva, { foreignKey: 'userId', as: 'reservas' });
-// Reserva.belongsTo(User, { foreignKey: 'userId', as: 'usuario' });
-
-// Sala.hasMany(Reserva, { foreignKey: 'salaId', as: 'reservas' });
-// Reserva.belongsTo(Sala, { foreignKey: 'salaId', as: 'sala' });
-
-// module.exports = { sequelize, User, Sala, Reserva };
-
 const sequelize = require('../config/database');
 const User = require('./User');
 const Sala = require('./Sala');
 const Reserva = require('./Reserva');
-const Chamado = require('./Chamado');
-const ComentarioChamado = require('./ComentarioChamado');
 const Impressora = require('./Impressora');
 const ManutencaoImpressora = require('./ManutencaoImpressora');
 const BaseConhecimento = require('./BaseConhecimento');
@@ -32,22 +15,6 @@ User.hasMany(Reserva, { foreignKey: 'userId', as: 'reservas' });
 Reserva.belongsTo(User, { foreignKey: 'userId', as: 'usuario' });
 Sala.hasMany(Reserva, { foreignKey: 'salaId', as: 'reservas' });
 Reserva.belongsTo(Sala, { foreignKey: 'salaId', as: 'sala' });
-
-// ── Chamados ──────────────────────────────────
-User.hasMany(Chamado, { foreignKey: 'abertoPorId', as: 'chamadosAbertos' });
-Chamado.belongsTo(User, { foreignKey: 'abertoPorId', as: 'abertoPor' });
-User.hasMany(Chamado, { foreignKey: 'tecnicoId', as: 'chamadosAtribuidos' });
-Chamado.belongsTo(User, { foreignKey: 'tecnicoId', as: 'tecnico' });
-Sala.hasMany(Chamado, { foreignKey: 'salaId', as: 'chamados' });
-Chamado.belongsTo(Sala, { foreignKey: 'salaId', as: 'sala' });
-Impressora.hasMany(Chamado, { foreignKey: 'impressoraId', as: 'chamados' });
-Chamado.belongsTo(Impressora, { foreignKey: 'impressoraId', as: 'impressora' });
-
-// ── Comentários Chamados ──────────────────────
-Chamado.hasMany(ComentarioChamado, { foreignKey: 'chamadoId', as: 'comentarios' });
-ComentarioChamado.belongsTo(Chamado, { foreignKey: 'chamadoId', as: 'chamado' });
-User.hasMany(ComentarioChamado, { foreignKey: 'userId', as: 'comentarios' });
-ComentarioChamado.belongsTo(User, { foreignKey: 'userId', as: 'usuario' });
 
 // ── Impressoras ────────────────────────────────
 Impressora.hasMany(ManutencaoImpressora, { foreignKey: 'impressoraId', as: 'manutencoes' });
@@ -78,7 +45,6 @@ ManutencaoPreventiva.belongsTo(User, { foreignKey: 'criadoPorId', as: 'criadoPor
 module.exports = {
   sequelize,
   User, Sala, Reserva,
-  Chamado, ComentarioChamado,
   Impressora, ManutencaoImpressora,
   BaseConhecimento, LogAuditoria,
   BloqueioSala, Comunicado, ManutencaoPreventiva
